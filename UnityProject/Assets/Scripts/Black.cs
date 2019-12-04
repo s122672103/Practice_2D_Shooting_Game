b1dcffd94c99118c529de102aa5034a0320abe5b
OLIVER  HSU <s122672103@gmail.com>
2019-12-04 18:10:32 +0800

    射擊、受傷方法

--- UnityProject/Assets/Scripts/Black.cs
+++ UnityProject/Assets/Scripts/Black.cs
@@ -4,6 +4,33 @@ using UnityEngine.UI;
 public class Black : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    [Header("血量")]
+    public int Hp = 10;
+    [Header("血量介面")]
+    public Text TextHp;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip SoundHit;
+
+    public void Hurt() {
+
+        Hp = Hp - 1;
+        TextHp.text = Hp.ToString();
+        aud.PlayOneShot(SoundHit,1f);
+
+        if (Hp <= 0)
+        {
+            Destroy(gameObject, 0.12f);
+        }
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if (collision.gameObject.tag == "子彈") {
+            Hurt();
+        }
+    }
+
     #endregion
 }
--- UnityProject/Assets/Scripts/White.cs
+++ UnityProject/Assets/Scripts/White.cs
@@ -3,8 +3,37 @@
 public class White : MonoBehaviour
 {
     #region 練習區域 - 在此區域內練習
-    
+
+    [Header("子彈")]
+    public GameObject bullet;
+    [Header("子彈發射位置")]
+    public Transform point;
+    [Header("音效區域")]
+    public AudioSource aud;
+    public AudioClip ShootSound;
+
+    /// <summary>
+    /// 射擊方法
+    /// </summary>
+    public void Shoot() {
+
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Vector3 pos = point.position;
+            Instantiate( bullet, pos, Quaternion.identity);
+            aud.PlayOneShot(ShootSound, 1f);
+        }
+    }
+
+
+
+
     #endregion
+
+     
+
+
+
 
     #region KID 區域 - 不要偷看 @3@
     [Header("移動速度"), Range(0f, 100f)]
@@ -20,6 +49,7 @@ public class White : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
+        Shoot();
     }
 
     /// <summary>
